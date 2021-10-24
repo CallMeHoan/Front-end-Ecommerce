68a55646b7be55b9913df5dabd05a481cf3f9189
Hoàn Nguyễn <softteam2021@gmail.com>
2021-10-25 06:45:51 +0700

    fix links for information and account

--- Components/Buyer/AccountSlideBar/AccountSlideBar.js
+++ Components/Buyer/AccountSlideBar/AccountSlideBar.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
         <h2 class="account__h2" style="text-transform: uppercase;">Account</h2>
         <ul class="list-unstyled components mb-5">
             <li>
-                <a class="account__links" href="#login">Login</a>
+                <a class="account__links" href="../../MyAccount/Login/index.html">Login</a>
             <li>
-                <a class="account__links" href="#register">Register</a>
+                <a class="account__links" href="../../MyAccount/Register/index.html">Register</a>
             </li>
             <li>
-                <a class="account__links" href="#myaccount">My Account</a>
+                <a class="account__links" href="../../MyAccount/Account/index.html">My Account</a>
             </li>
             <li>
-                <a class="account__links" href="#wishlist">Wish List</a>
+                <a class="account__links" href="../../MyAccount/WishList/index.html">Wishlist</a>
             </li>
             <li>
-                <a class="account__links" href="#orderhistory">Order History</a>
+                <a class="account__links" href="../../MyAccount/OrderHistory/index.html">Order History</a>
             </li>
         </ul>
     </div>
@@ -24,15 +24,16 @@ $(document).ready(function() {
         <h2 class="account__h2" style="text-transform: uppercase;">Information</h2>
         <ul class="list-unstyled components mb-5">
             <li>
-                <a class="account__links" href="#aboutus">About Us</a>
+                <a class="account__links" href="../../Information/AboutUs/index.html">About Us</a>
             <li>
-                <a class="account__links" href="#privacy">Privacy Policy</a>
+                <a class="account__links" href="../../Information/PrivacyPolicy/index.html">Privacy Policy</a>
             </li>
             <li>
-                <a class="account__links" href="#terms">Terms & Conditions</a>
+                <a class="account__links" href="../../Information/TearmAndConditions/index.html">Terms & Conditions</a>
             </li>
             <li>
-                <a class="account__links" href="contact">Contact Us</a>
+                <a class="account__links" href="../../Information/ContactUs/index.html">Contact Us</a>
+            </li>
         </ul>
     </div>
 </nav>
